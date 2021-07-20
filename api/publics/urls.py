8479216f01987ee12be390150715986548cb600e
박준영 <sjwm98@gmail.com>
2021-07-20 14:07:01 +0900

    Add: 컨벤션 및 로직 수정

--- api/publics/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import urlpatterns, routers
-from .views import PublicRetrieveModelMixin
-from django.urls import path
-
-
-urlpatterns = [
-    path('/<int:pk>', PublicRetrieveModelMixin.as_view())
-]
