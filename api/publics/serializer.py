ab64f8120188871271beb605ab5e9a3dc0174ba6
박준영 <sjwm98@gmail.com>
2021-07-20 14:11:03 +0900

    Add: public구현완료

---
+++ api/publics/serializer.py
@@ -0,0 +1,9 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import Public
+
+class PublicSerializer(ModelSerializer):
+
+    class Meta:
+        model  = Public
+        fields = ('number', 'cost')
