3cd595dab8f84780a8b074547da0f75d22bf97c4
박준영 <sjwm98@gmail.com>
2021-07-23 16:19:21 +0900

    add: docker compose

--- fa/urls.py
+++ fa/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('api/admin/user/', include('rest_auth.registration.urls')),
     path('api/rest-auth/', include('rest_auth.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api', include('api.urls')),
